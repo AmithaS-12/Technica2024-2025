605dfd81b99720f3db7ef0a77d79cbe4a75fbd09
Amitha Sabbani <samitha3980@gmail.com>
2024-10-26 15:59:22 -0400

    Stash temp updates

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <header className="App-header">
+
+        <h1>Escape Room!</h1>
+
+        <a>Alice in Wonderland</a>
+
+      </header>
+
+      <body>
+
+        <button type="button">Start</button>
+        
+
+      </body>
+
+    </div>
+
+  );
+
+}
+
+
+export default App;
+
