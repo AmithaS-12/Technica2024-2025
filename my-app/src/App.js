023cd3edbfcf7f2261b5422a17426ab64edfb577
tanvia09 <136640828+tanvia09@users.noreply.github.com>
2024-10-26 15:45:16 -0400

    Fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a>
           Escape Room! <br>
           </br> <a>Alice in Wonderland</a>
-        </a>
+          </a>
       </header>
       <body>
         <button type="button">Start</button>
-        <button onclick="startButtonFunction"></button>
-        <script>
-        function startButtonFunction() {
-          } 
-      </script>
       </body>
     </div>
   );
 }
+export default App;*/
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <header className="App-header">
+
+        <h1>Escape Room!</h1>
+
+        <a>Alice in Wonderland</a>
+
+      </header>
+
+      <div>
+
+        <button type="button">Start</button>
+
+      </div>
+
+    </div>
+
+  );
+
+}
+
+
 export default App;
+
+
