33dffe10613763aaea4d67cb1c70a0f4a1cb6f1b
tanvia09 <136640828+tanvia09@users.noreply.github.com>
2024-10-26 17:26:08 -0400

    added button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,38 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [content, setContent] = useState("You follow a mysterious rabbit, and it leads you here. Jump into the hole?");
+  const [buttonClickCount, setButtonClickCount] = useState(0);
+  function changeContent() {
+    setButtonClickCount(prevCount => {
+      const newCount = prevCount + 1;
+    console.log (buttonClickCount);
+    if (buttonClickCount === 1) {
+      setContent("A forest clearing awaits you...You see a door. Enter?");
+    } else if (buttonClickCount === 2) {
+      setContent("button click 2");
+    }
+    
+    return newCount; // Return the updated count to setButtonClickCount
+    });
+  }
 
   return (
-
     <div className="App">
-
       <header className="App-header">
-
         <h1>Escape Room!</h1>
-
         <a>Alice in Wonderland</a>
-
       </header>
 
-      <body>
-
-      <input onclick="change()" type="button" value="Open Curtain" id="myButton1"></input>
-      <script></script>
-
-
-      </body>
-
+      <main>
+        <button id="startButton" onClick={changeContent}>Yes</button>
+        <div id="startDiv">
+          <p>{content}</p>
+        </div>
+      </main>
     </div>
-
   );
-
 }
 
-
 export default App;
-
-
